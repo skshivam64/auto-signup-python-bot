eea1782726c43278f2196945a653730b6a68daa1
Shivam Kumar <engineeraccidental@gmail.com>
2017-06-10 02:03:56 +0530

    First Commit, gotta work with the sys package to customise it

---
+++ auto_signup.py
@@ -0,0 +1,53 @@
+import time
+
+import random
+
+from selenium import webdriver
+
+chrome_path = r"C:\Selenium\chromedriver.exe"
+
+number_of_signups = 0
+
+def gen_rand_un():
+    un = "2016JE00"
+    un += str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+    return un
+
+while 1:
+        un = gen_rand_un()
+
+        driver = webdriver.Chrome(chrome_path)
+        url = r"http://myshoutbox.tk/signup.php"
+        driver.get(url)
+        
+        fname = driver.find_element_by_id("fname1")
+        fname.send_keys("Hacker")
+
+        lname = driver.find_element_by_id("lname1")
+        lname.send_keys("Bro")
+
+        username = driver.find_element_by_id("username1")
+        username.send_keys(un)
+
+        password = driver.find_element_by_id("password1")
+        password.send_keys("Hello@123")
+
+        re_password = driver.find_element_by_id("repass1")
+        re_password.send_keys("Hello@123")
+
+        email = driver.find_element_by_id("email1")
+        email.send_keys("hacker" + un + "@gmail.com")
+
+        btn = driver.find_element_by_name("submit")
+        btn.click()
+
+        #Waiting to get registered:
+        time.sleep(20)
+
+        driver.quit()
+        
+        number_of_signups += 1
+        print("No. of Signups so far: {}".format(number_of_signups))
+        
+
+        
